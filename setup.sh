c295fce2a991c49b0ec5315a4b1c9fa331895334
Adam Dangoor <adamdangoor@gmail.com>
2020-03-04 10:56:55 +0000

    Remove YCM setup (YouCompleteMe)

--- setup.sh
+++ setup.sh
@@ -101,11 +101,6 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # Install vim plugins with `:PlugInstall`.
 nvim +PlugInstall +qall --headless
-# [Compile YouCompleteMe](https://github.com/Valloric/YouCompleteMe#mac-os-x)
-# with support for C-family languages.
-cd ~/.config/nvim/plug/YouCompleteMe/
-git submodule update --init --recursive
-python3 install.py --clang-completer
 
 git config --global push.default current
 
