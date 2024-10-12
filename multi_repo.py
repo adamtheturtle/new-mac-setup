d4481ab828948b256fc261bf8c4d44199db6aeda
Adam Dangoor <adamdangoor@gmail.com>
2024-10-12 15:32:36 +0100

    Update multi_repo.py

--- multi_repo.py
+++ multi_repo.py
@@ -21,18 +21,18 @@ def main():
 	fetch_args = ["git", "fetch", "origin"]
 	checkout_master_args = ["git", "checkout", "main"]
 	git_pull_args = ["git", "pull"]
-	checkout_args = ["git", "checkout", "-b", "yamlfix"]
+	checkout_args = ["git", "checkout", "-b", "vscode-test-settings"]
     # cp_args = ["cp", "../vws-cli/.github/workflows/dependabot-merge.yml", "./.github/workflows/dependabot-merge.yml"]
-	git_add_args = ["git", "add", "."]
-	git_commit_args = ["git", "commit", "--no-verify", "-am", "Remove yamlfmt config"]
-	git_push_args = ["git", "push", "--no-verify"]
+	git_add_args = ["git", "add", "pyproject.toml"]
+	git_commit_args = ["git", "commit", "-am", "Configure VSCode to run tests in its UI"]
+	git_push_args = ["git", "push"]
 	gh_pr_create_args = ["gh", "pr", "create", "--fill"]
 	gh_repo_view_args = ["gh", "pr", "view", "--web"]
 	all_args = [
 		fetch_args,
 		checkout_master_args,
-		["git", "reset", "--hard", "HEAD"],
-		["git", "config", "pull.rebase", "false"],
+#		["git", "reset", "--hard", "HEAD"],
+#		["git", "config", "pull.rebase", "false"],
 #		git_pull_args,
 #		checkout_args,
 #		["pre-commit", "autoupdate"],
@@ -44,6 +44,7 @@ def main():
 	]
 	for subdir_name in subdir_names:
 		subdir = base_dir / subdir_name
+		_delete_merged_branches(subdir=subdir)
 
 		for args in all_args:
 			try:
@@ -51,32 +52,31 @@ def main():
 			except subprocess.CalledProcessError as exc:
 				print(exc)
 				print("Exception in", subdir)
+
+def _delete_merged_branches(subdir: Path) -> None:
+	result = subprocess.run(
+		["git", "branch", "--merged"], 
+		capture_output=True, 
+		text=True, 
+		check=True,
+		cwd=subdir,
+	)
 	
-#	for subdir_name in subdir_names:
-#		subdir = base_dir / subdir_name
-#		result = subprocess.run(
-#			["git", "branch", "--merged"], 
-#			capture_output=True, 
-#			text=True, 
-#			check=True,
-#			cwd=subdir,
-#		)
-#		
-#		# Split the result into individual branch names
-#		branches = result.stdout.splitlines()
-#		
-#		# Filter out the current branch (marked with '*') and any excluded branches
-#		filtered_branches = [
-#			branch.strip() for branch in branches
-#			if not branch.startswith("*") and branch.strip() != "main"
-#		]
-#		
-#		if filtered_branches:
-#			subprocess.run(
-#				["git", "branch", "-d", *filtered_branches], 
-#				check=True,
-#				cwd=subdir,
-#			)
+	# Split the result into individual branch names
+	branches = result.stdout.splitlines()
+	
+	# Filter out the current branch (marked with '*') and any excluded branches
+	filtered_branches = [
+		branch.strip() for branch in branches
+		if not branch.startswith("*") and branch.strip() != "main"
+	]
+	
+	if filtered_branches:
+		subprocess.run(
+			["git", "branch", "-d", *filtered_branches], 
+			check=True,
+			cwd=subdir,
+		)
 
 if __name__ == "__main__":
 	main()
